f570a64cf33edce3a71c9e97a2c0e40e376afe26
Soren Geiger <soerengeiger@gmx.de>
2024-04-24 01:28:01 +0200

    added error handling for non .ber files

--- main.c
+++ main.c
@@ -6,11 +6,28 @@
 /*   By: sgeiger <sgeiger@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 17:54:43 by sgeiger           #+#    #+#             */
-/*   Updated: 2024/04/24 01:14:24 by sgeiger          ###   ########.fr       */
+/*   Updated: 2024/04/24 01:27:32 by sgeiger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	check_input(t_game *game, int argc, char *argv[])
+{
+	size_t	len;
+
+	len = ft_strlen(argv[1]);
+	if (argc != 2)
+	{
+		game->my_error = "Error: Input has to be: \"./game <map.ber>\"\n";
+		clean_exit(game);
+	}
+	if (len <= 4 || ft_strcmp(argv[1] + len - 4, ".ber") != 0)
+	{
+		game->my_error = "Error: Input file has to be \".ber\"\n";
+		clean_exit(game);
+	}
+}
 
 static void	check_success(t_game *game)
 {
@@ -31,11 +48,7 @@ int	main(int argc, char *argv[])
 	game.mlx = mlx_init(16, 16, "Your future gamedev!", false);
 	if (!game.mlx)
 		ft_mlx_error();
-	if (argc != 2)
-	{
-		game.my_error = "Error: Input has to be: \"./game <map.ber>\"\n";
-		clean_exit(&game);
-	}
+	check_input(&game, argc, argv);
 	init_map(&game, argv);
 	mlx_key_hook(game.mlx, key_hook, &game);
 	mlx_loop(game.mlx);
